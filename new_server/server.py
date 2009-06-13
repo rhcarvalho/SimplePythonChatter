f1122910b71f9b5de663080f0678c89c3dd4f421
Rodolfo Henrique Carvalho <rhcarvalho@gmail.com>
2009-06-12 23:21:06 -0300

    adding new_server

---
+++ new_server/server.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from twisted.internet import protocol, reactor, defer
+from twisted.protocols.basic import LineReceiver
+
+from users import UserManager
+
+
+_ = lambda m: m
+
+
+##
+##        status_message = (_("Currently there are %s users online and %s known "
+##                            "users.\nLast user connected at %s: UID=<%s>, IP=<%s>") %
+##                           (online_users, known_users, user.created, user.id, user.ip))
+
+
+
+class FingerProtocol(basic.LineReceiver):
+    def lineReceived(self, user):
+        self.factory.getUser(user
+        ).addErrback(lambda _: "Internal error in server"
+        ).addCallback(lambda m:
+        (self.transport.write(m+"\r\n"),
+        self.transport.loseConnection()))
+
+class FingerFactory(protocol.ServerFactory):
+    protocol = FingerProtocol
+    def __init__(self, **kwargs): self.users = kwargs
+    def getUser(self, user):
+        return defer.succeed(self.users.get(user, "No such user"))
+
+
+
+class SimpleChatProtocol(LineReceiver):
+
+    def connectionMade(self):
+        self.user = self.factory.newUser(self.transport.getPeer(), self.transport.write)
+
+        defer.Deferred().addCallback(
+            self.user.write, self.factory.welcome_message
+        ).addCallback(
+            self.announceNewUser, self.user
+        ).addCallback(
+            self.sendStats
+        ).addCallback(
+            self.factory.setStatusMessage
+        )
+
+    def connectionLost(self, reason):
+        pass
+
+    def lineReceived(self, line):
+        pass
+
+    def announceNewUser(self, user):
+        for online_user in self.factory.user_manager.online_users:
+            online_user.write("USER %s %s %s" % (user.id, user.name, user.ip))
+
+    def sendStats(self):
+        stats = self.factory.user_manager.stats
+        online_users = stats['online_users']
+        known_users = stats['known_users']
+        self.transport.write("STATS %s %s" % (online_users, known_users))
+
+
+class SimpleChatFactory(protocol.ServerFactory):
+    protocol = SimpleChatProtocol
+
+    def __init__(self, user_manager):
+        self.user_manager = user_manager
+        self.welcome_message = _("Welcome to Simple Python Chatter")
+
+    def newUser(self, peer, write):
+        user = self.user_manager.new()
+        user.ip = peer.host
+        user.write = write
+        return user
+
+    def setStatusMessage(self, last_user):
+        stats = self.factory.user_manager.stats
+        online_users = stats['online_users']
+        known_users = stats['known_users']
+        status_message = (_("Currently there are %s users online and %s known "
+                            "users.\nLast user connected at %s: UID=<%s>, IP=<%s>") %
+                           (online_users, known_users, last_user.created, last_user.id, last_user.ip))
+        print "[Status]", status_message
+
+
+reactor.listenTCP(6666, SimpleChatFactory(UserManager()))
+reactor.run()
---
+++ new_server/users.py
@@ -0,0 +1,64 @@
+﻿# -*- coding: utf-8 -*-
+from datetime import datetime
+
+
+class User(object):
+    """Represent an user"""
+    def __init__(self, id, name):
+        """Create a user"""
+        self.id = id
+        self.name = name
+        self.ip = None
+        self.created = datetime.now()
+
+    def write(self, message):
+        """Write a message to self"""
+
+
+class UserManager(object):
+    """Manage a group of users"""
+    __id_counter = 0
+
+    def __init__(self):
+        """Create a new UserManager"""
+        self.online_users = {}
+        self.offline_users = {}
+
+    def new(self, name=""):
+        """Return a new user registered under the given name."""
+        id = self.getID()
+        user = User(id, name)
+        self.offline_users[user.id] = user
+        return user
+
+    def login(self, id):
+        """Login an existing offline user registered under the given id.
+
+        Will pass silently if there is no offline user with the given id.
+        """
+        user = self.offline_users.pop(id, None)
+        if user is not None:
+            self.online_users[user.id] = user
+
+    def logout(self, id):
+        """Logout an existing online user registered under the given id.
+
+        Will pass silently if there is no online user with the given id.
+        """
+        user = self.online_users.pop(id, None)
+        if user is not None:
+            self.offline_users[user.id] = user
+
+    @property
+    def stats(self):
+        online_users = len(self.online_users)
+        offline_users = len(self.offline_users)
+        known_users = online_users + offline_users
+        return dict(online_users=online_users,
+                    offline_users=offline_users,
+                    known_users=known_users)
+
+    def getID(self):
+        """Return an appropriate id for a new user"""
+        self.__id_counter += 1
+        return self.__id_counter
